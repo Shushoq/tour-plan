fa2176bf2889b9572b492a3c788c74f1d282d950
shushoq <wywkoff@gmail.com>
2020-11-16 10:05:15 +0300

    create: header section

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+// const burger = document.querySelector('.burger');
+// const menu = document.querySelector('.menu');
+// const closeElem = document.querySelector('.menu__close');
+
+// burger.addEventListener('click', () => {
+//   menu.classList.add('menu_active');
+// })
+
+// closeElem.addEventListener('click', () => {
+//   menu.classList.remove('menu_active');
+// })
