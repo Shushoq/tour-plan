85eef8bd9da48a8eb6d13ca1741f005d3e903968
shushoq <wywkoff@gmail.com>
2020-11-24 11:54:20 +0300

    create: added swiper-slider

--- src/js/main.js
+++ src/js/main.js
@@ -9,3 +9,25 @@
 // closeElem.addEventListener('click', () => {
 //   menu.classList.remove('menu_active');
 // })
+import Swiper, {
+  Navigation,
+  Pagination
+} from "swiper";
+Swiper.use([Navigation, Pagination]);
+const swiper = new Swiper(".hotel__slider", {
+  loop: true,
+  speed: 500,
+  effect: 'fade',
+  fade: {
+    crossFade: true
+  },
+  navigation: {
+    nextEl: ".hotel__slider-btn--next",
+    prevEl: ".hotel__slider-btn--prev",
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+});
