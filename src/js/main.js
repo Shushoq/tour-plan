3e4cdd01d13cd2f07d44d02f9d745af62a94cb48
shushoq <wywkoff@gmail.com>
2020-12-04 22:45:53 +0300

    create: add form

--- src/js/main.js
+++ src/js/main.js
@@ -9,12 +9,7 @@ burger.addEventListener('click', () => {
   user.classList.toggle('header__user--active')
 })
 
-// closeElem.addEventListener('click', () => {
-//   menu.classList.remove('menu_active');
-// })
-
 import Swiper from "swiper";
-
 
 const hotelSwiper = new Swiper(".hotel__slider", {
   loop: true,
@@ -42,3 +37,36 @@ const reviewsSwiper = new Swiper(".reviews__slider", {
     prevEl: ".reviews__slider-btn--prev",
   },
 });
+
+
+function send(event, php) {
+  console.log("Отправка запроса");
+  event.preventDefault ? event.preventDefault() : event.returnValue = false;
+  let req = new XMLHttpRequest();
+  req.open('POST', php, true);
+  req.onload = function () {
+    if (req.status >= 200 && req.status < 400) {
+      json = JSON.parse(this.response);
+      console.log(json);
+      if (json.result == "success") {
+        alert("Сообщение отправлено");
+      } else {
+        alert("Ошибка. Сообщение не отправлено");
+      }
+    } else {
+      alert("Ошибка сервера. Номер: " + req.status);
+    }
+  };
+  req.onerror = function () {
+    alert("Ошибка отправки запроса");
+  };
+  req.send(new FormData(event.target));
+}
+
+
+const maps = document.querySelector('.hotel__map')
+maps.addEventListener('click', () => {
+  maps.innerHTML = `<iframe class="hotel__map-frame"
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7500.746811551891!2d-122.42303962194119!3d37.774205101819256!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808580a196ede719%3A0x2fb5607634331f2f!2zSGF5ZXMgVmFsbGV5LCDQodCw0L0t0KTRgNCw0L3RhtC40YHQutC-LCDQmtCw0LvQuNGE0L7RgNC90LjRjyA5NDEwMiwg0KHQqNCQ!5e0!3m2!1sru!2sru!4v1606217945560!5m2!1sru!2sru"
+  style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>`
+})
